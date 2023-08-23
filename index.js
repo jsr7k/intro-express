cdd867d1a6355d40d4e143418dbeb574b3edee15
JSR7K <joserhzp@gmail.com>
2023-08-22 21:07:03 -0600

    Second commit

--- index.js
+++ index.js
@@ -2,11 +2,34 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const users = [
+    { id: 1, name: 'Rodrigo', lastName: 'Cabrera'},
+    { id: 2, name: 'Juan', lastName: 'Perez'},
+    { id: 3, name: 'Pedro', lastName: 'Gonzalez'},
+]
+
 app.get('/', (request, response) => {
     response.send({ message: 'Intro ExpressJS 26a'})
 })
 
+app.get('/users', (request, response) => {
+    
+    response.send(users)
+})
+
+app.get('/users/:id', (request, response) => {
+    console.log(request.params);
+    const userId = request.params.id
+    
+    const findUser = users.find(user => user.id == userId)
+
+    if (!findUser) {
+        response.send({ error: `Usuario con el id ${userId} no encontrado`})
+    }
+    response.send(findUser)
+})
+
 
 app.listen(port, () => {
-    console.log('Aplicación escuchando por el puerto ' + port)
+    console.log(`Aplicación escuchando por el puerto ${port}`)
 })
